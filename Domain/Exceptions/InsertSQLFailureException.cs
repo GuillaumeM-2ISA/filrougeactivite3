9f7c1eb926ce39ba5a9553803ad199a1cab59515
GuillaumeM-2ISA <mourlonguillaume@gmail.com>
2022-09-26 10:54:32 +0200

    Préparation exceptions SQL domain

---
+++ Domain/Exceptions/InsertSQLFailureException.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public class InsertSQLFailureException : Exception
+    {
+        public InsertSQLFailureException(Entity entity) : base($"L'insertion de l'entité {entity}")
+        {
+
+        }
+    }
+}
---
+++ Domain/Exceptions/UpdateSQLFailureException.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public class UpdateSQLFailureException : Exception
+    {
+        public UpdateSQLFailureException(Entity entity) : base($"L'update de l'entité {entity} à échouée")
+        {
+
+        }
+    }
+}
