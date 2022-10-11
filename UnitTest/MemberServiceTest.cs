ad9483a96049d829bedb79bae15b51f0bc918fcb
GuillaumeM-2ISA <mourlonguillaume@gmail.com>
2022-10-11 09:23:41 +0200

    MAJ tests unitaire memberservicetest 4

--- UnitTest/MemberServiceTest.cs
+++ UnitTest/MemberServiceTest.cs
@@ -86,5 +86,77 @@ namespace UnitTest
             //Assert
             Assert.Equal(expectedContent, member);
         }
+
+        [Fact]
+        public async void RegisterAsyncShouldBeOk()
+        {
+            //Arrange
+            IUnitOfWork dbContext = Mock.Of<IUnitOfWork>();
+            IMemberRepository Members = Mock.Of<IMemberRepository>();
+            Mock.Get(dbContext)
+                .Setup(dbContext => dbContext.Members)
+                .Returns(Members);
+
+            Member newMember = new Member()
+            {
+                Nickname = "tata",
+                Email = "tata@toto.fr",
+                Password = "tatapassword"
+            };
+
+            Member newMemberWithPasswordEncrypted = new Member()
+            {
+                Nickname = "tata",
+                Email = "tata@toto.fr",
+                Password = "txn1kSaxajNgSQ5Fynth8XYY/DSeZal+iK2YmGUmG38="
+            };
+
+            Member expectedContent = new Member() { Id = 3, Nickname = "tata", Type = "Member", Email = "tata@toto.fr", Password = "txn1kSaxajNgSQ5Fynth8XYY/DSeZal+iK2YmGUmG38=", CreatedAt = DateTime.Now };
+
+            MemberService memberService = new MemberService(dbContext);
+
+            Mock.Get(Members).Setup(Members => Members.AddAsync(newMemberWithPasswordEncrypted)).ReturnsAsync(expectedContent);
+
+            //Act
+            Member member = await memberService.RegisterAsync(newMember);
+
+            //Assert
+            Assert.Equal(expectedContent, member);
+        }
+
+        [Fact]
+        public async void UpdatePasswordAsyncShouldBeOk()
+        {
+            //Arrange
+            IUnitOfWork dbContext = Mock.Of<IUnitOfWork>();
+            IMemberRepository Members = Mock.Of<IMemberRepository>();
+            Mock.Get(dbContext)
+                .Setup(dbContext => dbContext.Members)
+                .Returns(Members);
+
+            Member modifiedMember = new Member()
+            {
+                Id = 2,
+                Password = "titipassword"
+            };
+
+            Member modifiedMemberWithPasswordEncrypted = new Member()
+            {
+                Id = 2,
+                Password = "4vtbZCx1En4VqfCnDyfYitoW+dlLjaMppEpqsaHyKCo="
+            };
+
+            Member expectedContent = new Member() { Id = 2, Nickname = "titi", Type = "Member", Email = "titi@toto.fr", Password = "4vtbZCx1En4VqfCnDyfYitoW+dlLjaMppEpqsaHyKCo=", CreatedAt = DateTime.Parse("11/10/2022 08:51:59"), UpdateAt = DateTime.Now };
+
+            MemberService memberService = new MemberService(dbContext);
+
+            Mock.Get(Members).Setup(Members => Members.UpdatePasswordAsync(modifiedMemberWithPasswordEncrypted)).ReturnsAsync(expectedContent);
+
+            //Act
+            Member member = await memberService.UpdatePasswordAsync(modifiedMember);
+
+            //Assert
+            Assert.Equal(expectedContent, member);
+        }
     }
 }
