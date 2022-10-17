9ee42f7666531ab5f2c138acec00271c6f7101e7
GuillaumeM-2ISA <mourlonguillaume@gmail.com>
2022-10-17 14:43:49 +0200

    MAJ ajout login form

--- WinForms/Form1.Designer.cs
+++ WinForms/Form1.Designer.cs
@@ -29,20 +29,229 @@ namespace WinForms
         /// </summary>
         private void InitializeComponent()
         {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnRelaxZone = new System.Windows.Forms.Button();
+            this.btnQuestions = new System.Windows.Forms.Button();
+            this.btnAddress = new System.Windows.Forms.Button();
+            this.btnDev = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabAdd = new System.Windows.Forms.TabPage();
+            this.tabEdit = new System.Windows.Forms.TabPage();
+            this.tabDelete = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabAdd.SuspendLayout();
+            this.tabEdit.SuspendLayout();
+            this.tabDelete.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnRelaxZone);
+            this.splitContainer1.Panel1.Controls.Add(this.btnQuestions);
+            this.splitContainer1.Panel1.Controls.Add(this.btnAddress);
+            this.splitContainer1.Panel1.Controls.Add(this.btnDev);
+            this.splitContainer1.Panel1.Controls.Add(this.btnConnect);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Panel2.Controls.Add(this.btnRefresh);
+            this.splitContainer1.Size = new System.Drawing.Size(957, 582);
+            this.splitContainer1.SplitterDistance = 183;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btnRelaxZone
+            // 
+            this.btnRelaxZone.Location = new System.Drawing.Point(15, 360);
+            this.btnRelaxZone.Name = "btnRelaxZone";
+            this.btnRelaxZone.Size = new System.Drawing.Size(152, 29);
+            this.btnRelaxZone.TabIndex = 4;
+            this.btnRelaxZone.Text = "Espace détente";
+            this.btnRelaxZone.UseVisualStyleBackColor = true;
+            // 
+            // btnQuestions
+            // 
+            this.btnQuestions.Location = new System.Drawing.Point(15, 310);
+            this.btnQuestions.Name = "btnQuestions";
+            this.btnQuestions.Size = new System.Drawing.Size(152, 29);
+            this.btnQuestions.TabIndex = 3;
+            this.btnQuestions.Text = "Questions diverses";
+            this.btnQuestions.UseVisualStyleBackColor = true;
+            // 
+            // btnAddress
+            // 
+            this.btnAddress.Location = new System.Drawing.Point(15, 263);
+            this.btnAddress.Name = "btnAddress";
+            this.btnAddress.Size = new System.Drawing.Size(152, 29);
+            this.btnAddress.TabIndex = 2;
+            this.btnAddress.Text = "Adresses utiles";
+            this.btnAddress.UseVisualStyleBackColor = true;
+            // 
+            // btnDev
+            // 
+            this.btnDev.Location = new System.Drawing.Point(15, 216);
+            this.btnDev.Name = "btnDev";
+            this.btnDev.Size = new System.Drawing.Size(152, 29);
+            this.btnDev.TabIndex = 1;
+            this.btnDev.Text = "Développement";
+            this.btnDev.UseVisualStyleBackColor = true;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(15, 53);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(152, 29);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Se connecter";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(124, 39);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(94, 29);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Supprimer";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(503, 39);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(94, 29);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "Modifier";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(502, 39);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 29);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Ajouter";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(653, 492);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(94, 29);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Actualiser";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(728, 406);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabAdd);
+            this.tabControl1.Controls.Add(this.tabEdit);
+            this.tabControl1.Controls.Add(this.tabDelete);
+            this.tabControl1.Location = new System.Drawing.Point(19, 424);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(628, 137);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabAdd
+            // 
+            this.tabAdd.Controls.Add(this.btnAdd);
+            this.tabAdd.Location = new System.Drawing.Point(4, 29);
+            this.tabAdd.Name = "tabAdd";
+            this.tabAdd.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAdd.Size = new System.Drawing.Size(620, 104);
+            this.tabAdd.TabIndex = 0;
+            this.tabAdd.Text = "Ajouter";
+            this.tabAdd.UseVisualStyleBackColor = true;
+            // 
+            // tabEdit
+            // 
+            this.tabEdit.Controls.Add(this.btnEdit);
+            this.tabEdit.Location = new System.Drawing.Point(4, 29);
+            this.tabEdit.Name = "tabEdit";
+            this.tabEdit.Padding = new System.Windows.Forms.Padding(3);
+            this.tabEdit.Size = new System.Drawing.Size(620, 104);
+            this.tabEdit.TabIndex = 1;
+            this.tabEdit.Text = "Modifier";
+            this.tabEdit.UseVisualStyleBackColor = true;
+            // 
+            // tabDelete
+            // 
+            this.tabDelete.Controls.Add(this.btnDelete);
+            this.tabDelete.Location = new System.Drawing.Point(4, 29);
+            this.tabDelete.Name = "tabDelete";
+            this.tabDelete.Padding = new System.Windows.Forms.Padding(3);
+            this.tabDelete.Size = new System.Drawing.Size(620, 104);
+            this.tabDelete.TabIndex = 2;
+            this.tabDelete.Text = "Supprimer";
+            this.tabDelete.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(957, 582);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabAdd.ResumeLayout(false);
+            this.tabEdit.ResumeLayout(false);
+            this.tabDelete.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnRelaxZone;
+        private System.Windows.Forms.Button btnQuestions;
+        private System.Windows.Forms.Button btnAddress;
+        private System.Windows.Forms.Button btnDev;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabAdd;
+        private System.Windows.Forms.TabPage tabEdit;
+        private System.Windows.Forms.TabPage tabDelete;
     }
 }
 
