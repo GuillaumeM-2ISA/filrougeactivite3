556d567374706ba2cece94a8faccaa46eb3cae69
GuillaumeM-2ISA <mourlonguillaume@gmail.com>
2022-11-08 11:57:06 +0100

    MAJ 1er viewmodel

---
+++ 2isaForumAppMobile/MVVM/ViewsModels/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2isaForumAppMobile
+{
+    /// <summary>
+    /// Classe de base des ViewModel qui implémente l'interface 'INotifyPropertyChanged'
+    /// </summary>
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        // Implémentation de l'interface 'INotifyPropertyChanged'
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Méthode qui va émettre l'événement 'PropertyChanged' pour les Views. Le nom de 
+        // la propriété est passé par le système (c'est le nom de l'appelant)
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ 2isaForumAppMobile/MVVM/ViewsModels/categoryVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2isaForumAppMobile
+{
+    class CategoryVM : ViewModelBase
+    {
+        private string _titre = "Mon titre";
+        public string Titre
+        {
+            get
+            {
+                return _titre;
+            }
+
+            set
+            {
+                _titre = value;
+                RaisePropertyChanged(); // Permet de notifier la View d'une modification
+            }
+        }
+    }
+}
